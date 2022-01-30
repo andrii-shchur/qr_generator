6a009c9f444987522141654147d253f66d942745
Andrii <sparrow.andriy@gmail.com>
2022-01-30 21:09:39 +0200

    created models

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=64)
+    email = models.EmailField()
+    password = models.BinaryField(max_length=60)
+    created_at = models.DateTimeField()
+
+
+class QrCode(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
