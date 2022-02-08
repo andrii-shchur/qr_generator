0a4e07acd3a2c8a4849460040fb55a08d5eb8f2f
Andrii <sparrow.andriy@gmail.com>
2022-02-08 18:57:18 +0200

    bugfix

--- qr_generator/settings.py
+++ qr_generator/settings.py
@@ -112,7 +112,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/backend/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
