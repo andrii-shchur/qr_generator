15e383a215165b3f4e83c0e5c3fdca20e301af86
Andrii <sparrow.andriy@gmail.com>
2022-02-08 19:20:07 +0200

    deploy

--- qr_generator/settings.py
+++ qr_generator/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['qr-code-generator11.herokuapp.com', '192.168.0.110', '127.0.0.1']
 
